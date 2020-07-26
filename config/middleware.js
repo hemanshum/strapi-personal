07a7619c03b5778b7867b13b4e8488a6c4a9104b
Hemanshu Mahajan <hemanshu.mahajan@gmail.com>
2020-07-26 11:14:59 +0530

    added cors

--- config/middleware.js
+++ config/middleware.js
@@ -2,7 +2,7 @@ module.exports = {
   //...
   settings: {
     cors: {
-      origin: "http://localhost","https://appx-strapi-srv.herokuapp.com"
+      origin: ("http://localhost", "https://appx-strapi-srv.herokuapp.com"),
     },
   },
 };
